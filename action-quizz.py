4bdcd7afda1531a3bab53a73b92838f456c03f49
Gunaskar <gunasekar.thambusamy@gmail.com>
2018-10-29 01:57:14 +0000

    change using template

--- action-quizz.py
+++ action-quizz.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python2
-# -*-: coding utf-8 -*-
+# -*- coding: utf-8 -*-
 
+import ConfigParser
 from hermes_python.hermes import Hermes
-import times_tables as tt
-import json
+from hermes_python.ontology import *
+import io
 
-
-MQTT_IP_ADDR = "localhost"
-MQTT_PORT = 1883
-MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
-
-
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
 INTENT_START_MISSING_MILES_FAQ = "claim_miles_query_start"
 INTENT_MILES_MISSING_FLIGHT = "claim_miles_flown_flight"
 INTENT_INTERRUPT = "interrupt"
@@ -21,6 +18,26 @@ INTENT_FILTER_GET_ANSWER = [
     INTENT_INTERRUPT,
     INTENT_DOES_NOT_KNOW
 ]
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
+
+
 
 def start_missing_miles_faq(hermes, intent_message):
     print("missing_miles_faq start")
@@ -53,12 +70,16 @@ def session_ended(hermes, session_ended_message):
     print("Session Ended")
 
 
-with Hermes(MQTT_ADDR) as h:
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent(INTENT_START_MISSING_MILES_FAQ, start_missing_miles_faq) \
+         .subscribe_intent(INTENT_INTERRUPT, user_quits) \
+         .subscribe_intent(INTENT_DOES_NOT_KNOW, user_does_not_know) \
+         .subscribe_intent(INTENT_MILES_MISSING_FLIGHT, user_gives_answer) \
+         .subscribe_session_ended(session_ended) \
+         .subscribe_session_started(session_started) \
+         .start()
 
-    h.subscribe_intent(INTENT_START_MISSING_MILES_FAQ, start_missing_miles_faq) \
-        .subscribe_intent(INTENT_INTERRUPT, user_quits) \
-        .subscribe_intent(INTENT_DOES_NOT_KNOW, user_does_not_know) \
-        .subscribe_intent(INTENT_MILES_MISSING_FLIGHT, user_gives_answer) \
-        .subscribe_session_ended(session_ended) \
-        .subscribe_session_started(session_started) \
-        .start()
+
+
+
